a9579ed4d617d779e6a8c897780bb714af9d4420
Suliman Fayyaz <hydrajin@gmail.com>
2022-08-17 21:10:20 -0400

    Add shortened arrow statement

--- use_arrow_functions_to_write_concise_anonymous_functions.js
+++ use_arrow_functions_to_write_concise_anonymous_functions.js
@@ -2,4 +2,8 @@ const magic = () => {
   return new Date();
 };
 
+// const magic = () => new Date();
+// No need for return statement since arrow function has an implicit return
+
 console.log(magic());
+
